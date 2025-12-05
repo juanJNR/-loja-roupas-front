3c24f754daccee43754c56729d741b5f899ed609
juan nunes <juanrosanuness@gmail.com>
2025-12-05 16:54:59 -0300

    Fix: TypeScript

--- src/Detalhes.tsx
+++ src/Detalhes.tsx
@@ -166,9 +166,9 @@ export default function Detalhes() {
                 {produto.cor}
               </span>
             )}
-            {produto?.estoque > 0 ? (
+            {produto?.estoque && produto.estoque > 0 ? (
               <span className="bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
-                Em estoque: {produto.estoque}
+                Em estoque: {produto?.estoque}
               </span>
             ) : (
               <span className="bg-red-100 text-red-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
--- src/utils/ClienteType.ts
+++ src/utils/ClienteType.ts
@@ -2,4 +2,6 @@ export type ClienteType = {
     id: string
     nome: string
     email: string
+    cidade?: string
+    createdAt?: string
 }
